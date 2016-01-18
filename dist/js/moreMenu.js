1ebff2afeaa105bd9e3571875621608eacfff4db
Chris Pattle <chris.pattle@gmail.com>
2016-01-18 23:32:26 +0000

    Adding the ability to specify a minimum width before the menu kicks in

--- dist/js/moreMenu.js
+++ dist/js/moreMenu.js
@@ -7,7 +7,7 @@
 	var MoreMenu = function (element, options) {
 		this.$element = $(element);
 		this.totalWidth = 0;
-		this.containerWidth = $(element).width();
+		this.containerWidth = this.$element.width();
 		this.menuWidth = 0;
 		this.hiddenWidth = 0;
 		this.visibleWidth = 0;
@@ -15,18 +15,30 @@
 		this.keys = new Array();
 		this.items = new Array();
 		this.lastWindowWidth = $(window).width();
+		this.uninited = true;
 		this.options = $.extend({}, MoreMenu.DEFAULTS, options);
-		this.init();
+
+		if($(window).width() > this.options.minWidth) {
+			this.init();
+		}
+
+		$that = this;
+		$(window).bind('resize',function(e) {
+			$that.recalculate($that);
+		});
 	};
 
 	MoreMenu.DEFAULTS = {
+		minWidth: 0,
 		menuClass: 'mm-menu',
 		menuTemplate: '<li class="mm-menu"><a href="#">More</a><ul class="mm-dropdown-menu"></ul></li>',
 		dropdownMenuClass: 'mm-dropdown-menu'
 	};
 
 	MoreMenu.prototype.init = function() {
-		$that = this
+		$that = this;
+		this.uninited = false;
+
 		this.$element.find("> li").each(function(index) {
 			$that.totalWidth += $(this).outerWidth();
 			$that.visibleWidth += $(this).outerWidth();
@@ -46,10 +58,6 @@
 		this.totalWidth += 30;
 
 		this.down();
-
-		$(window).bind('resize',function(e) {
-			$that.recalculate($that);
-		});
 	}
 
 	MoreMenu.prototype.down = function() {
@@ -92,36 +100,60 @@
 	}
 
 	MoreMenu.prototype.reinit = function() {
-		$that = this
+		$that = this;
 		this.$element.find("> li:not(.mm-menu)").each(function(index) {
 			$that.items[$(this).children('a').text()] = {itemwidth: $(this).outerWidth(), itemelement: $(this)};
 		});
 
-		//Reverse the array because we want to be counting from the last nav item not the first
+		this.keys = new Array();
 		for(var k in this.items) {
 			this.keys.unshift(k);
 		}
 	}
 
+	MoreMenu.prototype.uninit = function() {
+		this.uninited = true;
+		this.$element.find('.mm-menu').remove();
+
+		this.$element.find("> li").each(function(index) {
+			$(this).show();
+		});
+
+		this.totalWidth = 0;
+		this.containerWidth = this.$element.width();
+		this.menuWidth = 0;
+		this.hiddenWidth = 0;
+		this.visibleWidth = 0;
+		this.lastElementVisble = -1;
+	}
+
 	MoreMenu.prototype.recalculate = function() {
-		//Update the container width
-		if(this.$element.outerWidth() != this.containerWidth) {
-			this.containerWidth = this.$element.outerWidth();
-			this.reinit();
-		}
+		if(this.options.minWidth !== 0 && window.matchMedia('(max-width: ' + this.options.minWidth + 'px)').matches) {
+			this.uninit();
+		} else {
+			if(this.uninited === true) {
+				this.init()
+			} else {
+				//Update the container width
+				if(this.$element.outerWidth() != this.containerWidth) {
+					this.containerWidth = this.$element.outerWidth();
+					this.reinit();
+				}
 
-		//Check to see if the window is reducing in width
-		if($(window).width() < this.lastWindowWidth) {
-			this.down();
-		}
+				//Check to see if the window is reducing in width
+				if($(window).width() < this.lastWindowWidth) {
+					this.down();
+				}
 
-		//Check to see if the window is increasing in width
-		if($(window).width() > this.lastWindowWidth) {
-			this.up();
-		}
+				//Check to see if the window is increasing in width
+				if($(window).width() > this.lastWindowWidth) {
+					this.up();
+				}
 
-		//Store the new window width so we can compare against it when the window is resized again
-		this.lastWindowWidth = $(window).width();
+				//Store the new window width so we can compare against it when the window is resized again
+				this.lastWindowWidth = $(window).width();
+			}
+		}
 	}
 
 	$.fn.moreMenu = function (option) {
